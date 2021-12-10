0ed12ecd75b61b5ef727b5cf7b69af28ca723be1
Mendy <mendyw94@gmail.com>
2021-12-10 17:46:39 -0500

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Your code here
+
+const dodger = document.getElementById('dodger');
+
+//dodger.style.bottom = "100px";
+//const movingLeft = dodger.style.left.replace("px", "");
+//const convertToNumber = parseInt(movingLeft, 10);
+
+function moveDodgerLeft() {
+    const movingLeft = dodger.style.left.replace("px", "");
+    const convertToNumber = parseInt(movingLeft, 10);
+
+    if (convertToNumber > 0){
+        dodger.style.left = `${convertToNumber - 1}px`;
+    }
+}
+
+function moveDodgerRight() {
+    const movingLeft = dodger.style.left.replace("px", "");
+    const convertToNumber = parseInt(movingLeft, 10);
+
+    if (convertToNumber < 360){
+        dodger.style.left = `${convertToNumber + 1}px`;
+    }
+}
+document.addEventListener("keydown", function (e) { 
+    if (e.key === 'ArrowLeft') {
+        moveDodgerLeft();
+    } else if (e.key === 'ArrowRight') {
+        moveDodgerRight();
+    }
+});
+
+
